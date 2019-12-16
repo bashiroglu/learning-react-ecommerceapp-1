54583e9d7ee6db021ae5146d3cef113062332d50
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-16 10:43:45 +0100

    added starting files, bootstrap and font awesome

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="container">
+        <div className="row">
+          <div className="col-6"></div>
+          <div className="col-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
