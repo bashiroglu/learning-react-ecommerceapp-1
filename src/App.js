18bb685d36e34a1b57e66d7f70667364daa011f9
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-18 10:33:45 +0100

    notes about routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Router, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar/Navbar';
 import Card from './components/Card/Card';
 import Default from './components/Default/Default';
 import Details from './components/Details/Details';
-import Product from './components/Product/Product';
+// import Product from './components/Product/Product';
 import ProductList from './components/ProductList/ProductList';
 
 function App() {
@@ -16,10 +16,15 @@ function App() {
     <React.Fragment>
       <Navbar />
       <Switch>
-        <Route path="/" component={ProductList} />
+        {/* this don't work as intended because fist one matches all <Route path="/" component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/card" component={Card} />
+        <Route component={Default} />*/}
         <Route path="/details" component={Details} />
         <Route path="/card" component={Card} />
         <Route component={Default} />
+        <Route path="/" component={ProductList} /> {/* we use this teqnique to avoid that proplem */}
+        
       </Switch>
     </React.Fragment>
   );
