bbe0bae61147173ffc8c25c6a5dfc3eac56838db
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-02 12:53:22 +0100

    added my cart button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <nav className="navbar bg-primary navbar-expand-sm navbar-dark px-sm-5">
         {/* 
 https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
@@ -17,9 +17,17 @@ https://www.iconfinder.com/Makoto_msk */}
         </Link>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-            <Link to="/" className="nav-link">Products</Link>
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
           </li>
         </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus" />
+            my cart
+          </button>
+        </Link>
       </nav>
     );
   }
